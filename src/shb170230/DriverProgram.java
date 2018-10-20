59b7b2be1f1b35264335e270bbb08f3cf78bf295
Utkarsh Gandhi <utkarshgandhi96@gmail.com>
2018-10-20 13:14:04 -0500

    adding random elements

--- src/shb170230/DriverProgram.java
+++ src/shb170230/DriverProgram.java
@@ -6,15 +6,25 @@ public class DriverProgram {
     public static void main(String[] args) {
 
         RobinhoodHashing<Integer> map = new RobinhoodHashing();
-
-        for (int i = 0; i <= 2000; i++) {
+        /*System.out.println("Index for find " + map.find(87));
+        for (int i = 0; i <= 1000; i++) {
             map.add(i);
         }
         map.printTable();
-        for (int i = 50; i <= 100; i++) {
+        for (int i = 50; i <= 1000; i++) {
             map.remove(i);
-        }
+        }*/
         map.printTable();
+        Random r = new Random();
+        Integer[] integers = new Integer[1000];
+        for (int i = 0; i < integers.length; i++) {
+            integers[i] = r.nextInt();
+        }
+        Timer timer = new Timer();
+        timer.start();
+        System.out.println(RobinhoodHashing.distinctElements(integers));
+        timer.end();
+        System.out.println(timer);
     }
 
 
